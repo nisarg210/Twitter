0b2781ea2957f090ecddb18ba75e22b0eb4e0299
Nisarg Prajapati <nisarg.prajapati21101@gmail.com>
2024-06-18 19:06:11 -0400

    Add comments for controller

--- user-service/src/main/java/com/twitter/user_service/controllers/UserController.java
+++ user-service/src/main/java/com/twitter/user_service/controllers/UserController.java
@@ -9,15 +9,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller class for handling user-related operations.
+ */
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
     private final UserService userService;
 
+    /**
+     * Constructor injection for UserService.
+     *
+     * @param userService the UserService to be injected
+     */
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * Get user by ID.
+     *
+     * @param id the user ID
+     * @return ResponseEntity with UserDTO
+     */
     @GetMapping(path="/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Long id){
         UserDTO userDTO = userService.getUserById(id);
